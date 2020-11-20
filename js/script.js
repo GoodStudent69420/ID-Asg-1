f9e3a73f4b93e4c7ae1e54f10091931993176e9e
GoodStudent69420 <lolotan9@gmail.com>
2020-11-20 15:03:25 +0800

    did the product page

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+//login and sign up form
+var modal = document.getElementById('login'); 
+var modal2 = document.getElementById('signup'); 
+window.onclick = function(event) { 
+    if (event.target == modal || event.target == modal2) { 
+        modal.style.display = "none"; 
+        modal2.style.display = "none";
+    } 
+}
+
+
